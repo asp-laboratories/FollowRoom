2ed20162789f041b20df9c5a148650735b67e862
LuisDavidGR <ldavidgrdrmz@gmail.com>
2026-04-04 13:36:09 -0700

    feat: Implementando mejores mensajes de alerta, confirmacion y errores - 31

--- followroom_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ followroom_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AmbientLightPlugin.register(with: registry.registrar(forPlugin: "AmbientLightPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
