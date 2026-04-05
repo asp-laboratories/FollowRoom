156b1cd1f38b0c6892a4049acb19ab5336810a19
LuisDavidGR <ldavidgrdrmz@gmail.com>
2026-04-04 22:47:20 -0700

    feat: Evaluar la disponibilidad del salon - 35

--- followroom_flutter/lib/services/disponibilidad_service.dart
+++ followroom_flutter/lib/services/disponibilidad_service.dart
@@ -25,4 +25,24 @@ class DisponibilidadService {
       throw Exception('Error al cargar disponibilidad');
     }
   }
+
+  Future<List<Map<String, dynamic>>> getEstadosSalones(String fecha) async {
+    try {
+      var dio = Dio();
+      dio.options.baseUrl = baseUrl;
+      final response = await dio.get(
+        '/estado-salon/',
+        queryParameters: {'fecha': fecha},
+      );
+      if (response.statusCode == 200) {
+        List<dynamic> data = response.data;
+        return data.map((item) => Map<String, dynamic>.from(item)).toList();
+      } else {
+        throw Exception('Error al cargar estados de salones');
+      }
+    } catch (e) {
+      print('Error al cargar estados de salones: $e');
+      throw Exception('Error al cargar estados de salones');
+    }
+  }
 }
